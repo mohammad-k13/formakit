1ac6dd47e99a6961c5882b9e77a55c9a0005b051
mohammad-k13 <mohammadkeshavarz.a.13@gmail.com>
2026-02-20 23:40:48 +0330

    static component
    create simple sample of static component get config and render layout then render input and make name and type of input dynamic base on config

--- packages/formakit/src/index.tsx
+++ packages/formakit/src/index.tsx
@@ -8,10 +8,13 @@ import {
 } from "react-grid-layout";
 import "react-grid-layout/css/styles.css";
 import "react-resizable/css/styles.css";
+import { IFormBuilderConfig } from "./types";
+import { ResponsiveLayout, StaticLayout } from "./components";
 
-const index = () => {
+const FormBuilder = ({config}: IFormBuilderConfig) => {
     const { width, containerRef, mounted } = useContainerWidth();
     console.log("width", width);
+    const layoutType = config.type;
 
     const layout: GridLayoutProps["layout"] = [
         { i: "a", x: 0, y: 0, w: 1, h: 2, static: true },
@@ -26,7 +29,7 @@ const index = () => {
             { i: "c", x: 3, y: 0, w: 1, h: 2 },
         ],
         lg: [
-            { i: "a", x: 0, y: 0, w: 1, h: 2, },
+            { i: "a", x: 0, y: 0, w: 1, h: 2 },
             { i: "b", x: 1, y: 0, w: 16, h: 2 },
             { i: "c", x: 17, y: 0, w: 1, h: 2 },
         ],
@@ -37,7 +40,7 @@ const index = () => {
             ref={containerRef}
             style={{
                 width: "100%",
-                height: "500px",
+                height: "800px",
                 border: "1px solid #442",
                 borderRadius: "5px",
             }}
@@ -88,8 +91,35 @@ const index = () => {
                     </div>
                 </Responsive>
             )}
+
+            <div
+                style={{
+                    width: "80%",
+                    height: "1px",
+                    background: "#595959",
+                    marginBlock: "15px",
+                    marginInline:" auto"
+                }}
+            />
+
+            <div
+                style={{
+                    width: "95%",
+                    height: "400px",
+                    border: "1px solid #442",
+                    borderRadius: "5px",
+                    marginInline:"auto",
+                    padding: '5px'
+                }}
+            >
+                {layoutType === "responsive" ? (
+                    <ResponsiveLayout {...config} />
+                ) : (
+                    <StaticLayout {...config} />
+                )}
+            </div>
         </div>
     );
 };
 
-export default index;
+export default FormBuilder;
